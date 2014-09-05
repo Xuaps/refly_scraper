6eb74d91b33ffd90e3fef7adb2879fd8cecb1b86
David Vilchez <dvilchez@xuaps.com>
2014-09-05 11:10:03 +0200

    initial commit

---
+++ refly_scraper/items.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ReferenceItem(scrapy.Item):
+  name = scrapy.Field()
+  content = scrapy.Field()
+  path = scrapy.Field()
+  url = scrapy.Field()
---
+++ refly_scraper/settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for refly_scrap project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'refly_scraper'
+
+SPIDER_MODULES = ['refly_scraper.spiders']
+NEWSPIDER_MODULE = 'refly_scraper.spiders'
+ITEM_PIPELINES = {'refly_scraper.pipelines.ReflyPipeline': 100}
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'refly_scrap (+http://www.yourdomain.com)'
