18dbee8b5d84008dee02a9622d9f829b0ba07ed8
David Vilchez <dvilchez@xuaps.com>
2014-09-19 15:30:55 +0200

    added generic pipeline

--- refly_scraper/pipelines.py
+++ refly_scraper/pipelines.py
@@ -4,11 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import html2text
 
 class ReflyPipeline(object):
     def process_item(self, item, spider):
+    	item['docset'] = spider.name
+    	item['type'] = spider.resolveType(item['url'], item['name'])
+    	item['parsed_url'] = spider.getSlashUrl(item['path'], item['name'])
+    	item['parent'] = item['parsed_url'][0:item['parsed_url'].rfind('/')]
+    	item['content'] = html2text.html2text(item['content'])
         return item
+
 
 #     def close_spider(spider):
 #        spider.log('spider closed')
