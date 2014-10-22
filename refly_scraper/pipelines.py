572ea93dc0a86ea69142d16cb6d157ca8fe9c43a
David Vilchez <dvilchez@xuaps.com>
2014-10-22 12:34:01 +0200

    tuning links in markdown

--- refly_scraper/pipelines.py
+++ refly_scraper/pipelines.py
@@ -8,23 +8,26 @@ import html2text as html2text_orig
 import re
 
 class ReflyPipeline(object):
-    link_re = re.compile("(\[\d*\]: (?:[\.:?=/\w \-#\(\)~,\.;\\n]|(?:\[\[[a-zA-Z]*\]\]))*)\\n\\n")
+    link_re = re.compile("( *\[\d*\]: (?:[\.:?=/\w\-#~,\.; \(\)%]|(?:\\n))*)")
+    title = re.compile("(title=(?:\"[^\"]*\"|'[^']*'))")
 
     def process_item(self, item, spider):
         item['docset'] = spider.name
         item['type'] = spider.resolveType(item['url'], item['name'])
-        item['parsed_url'] = spider.getSlashUrl(item['path'], item['name'])
+        item['parsed_url'] = spider.getSlashUrl(item['path'], item['name']).replace(' ', '_')
         item['parent'] = item['parsed_url'][0:item['parsed_url'].rfind('/')]
         item['content'] = self.html2text(item['content'])
         return item
 
     def html2text(self, html):
         """use html2text but repair newlines cutting urls and fix errors in links with code inside"""
+        html = self.title.sub('', html)
         h = html2text_orig.HTML2Text()
         h.inline_links = False
+        h.bypass_tables = True
         txt = h.handle(html).replace('`[', '[`')
 
         for l in self.link_re.findall(txt):
-            txt = txt.replace(l, l.replace('\n(', ' (').replace('\n', ''))
+            txt = txt.replace(l, l.replace('\n', '').strip() + '\n\n')
 
         return txt
