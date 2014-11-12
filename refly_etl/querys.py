c185c767302c2679c83d8aa2992f87e36d73f9a6
David Vilchez <dvilchez@xuaps.com>
2014-11-12 11:41:14 +0100

    Changed upsert query. ParentId resolution had some errors if parents were inserted after children.

--- refly_etl/querys.py
+++ refly_etl/querys.py
@@ -8,8 +8,8 @@ def UpsertQuery( connection, insert_buffer, update_buffer ):
     for item in insert_buffer:
         connection.execute(
             """
-            INSERT INTO refs ( reference, type, docset, content, uri, parent_id)\
-            VALUES (%s, %s, %s, %s, %s, (SELECT id FROM refs WHERE uri=%s));
+            INSERT INTO refs ( reference, type, docset, content, uri, parent_uri)\
+            VALUES (%s, %s, %s, %s, %s, %s);
             """,
             item['reference'], item['type'], item['docset'], item['content'], item['uri'], item['parent']
         )
